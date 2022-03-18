a8c958108ec65e768c1cd4c357b5357dadf1e195
Kitchana Thomutok <kitchana.thamutok@slu.edu>
2022-03-18 12:51:59 -0500

    change alphabet and config

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
-  language: 'YourLanguageHere', // This changes the display name for your language
+  language: 'Kreyol', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: 'YourNameHere', // Put your name here so people know who made this Wordle!
   authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
